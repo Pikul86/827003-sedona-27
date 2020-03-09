cbbce898cb5b9f4e34fad10cd782b05ba68088e4
Pikul86 <pikuliurii@gmail.com>
2020-03-09 17:34:18 +0700

    Исправленны ошибки

--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,10 @@ var link = document.querySelector(".button-search");
 var popup = document.querySelector(".modal");
 
 
+
+popup.classList.add("modal-close");
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.toggle("modal-show");
+    popup.classList.toggle("modal-close");
 });
 
